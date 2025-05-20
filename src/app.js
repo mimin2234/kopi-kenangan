1f139721a10b3e98d73939001c645c82715d7276
mimin2234 <muhammadmuslymin9333@gmail.com>
2025-05-20 12:17:43 +0800

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -17,18 +17,19 @@ document.addEventListener("alpine:init", () => {
       // cek apakah ada barang yang sama
       const cartItem = this.items.find((item) => item.id === newItem.id);
 
-      // jika belum ada item dalam cart
+      //  jika belum ada/ masih kosong
       if (!cartItem) {
         this.items.push({ ...newItem, quantity: 1, total: newItem.price });
         this.quantity++;
         this.total += newItem.price;
       } else {
-        // Jika barang sudah ada, update quantity dan total
+        // jika barang belum ada, cek apakah barang beda atau sama dengan yang ada di cart
         this.items = this.items.map((item) => {
+          // jika barang berbeda
           if (item.id !== newItem.id) {
             return item;
           } else {
-            // Update item yang sudah ada
+            // jika barang sudah ada, tambah quantity dan totalnya
             item.quantity++;
             item.total = item.price * item.quantity;
             this.quantity++;
@@ -38,35 +39,95 @@ document.addEventListener("alpine:init", () => {
         });
       }
     },
+
     remove(id) {
-      // ambil item yang mau di remove berdasarkan idnya
+      // ambil item yamg mau di remove berdasarkan id nya
       const cartItem = this.items.find((item) => item.id === id);
 
-      // jika item lebih dari 1
+      // jika lebih dari 1
       if (cartItem.quantity > 1) {
-        // telusuri 1
-        this.items = this.items.map((item) => {
-          if (item.id !== id) {
-            return item;
-          } else {
-            item.quantity--;
-            item.total = item.price * item.quantity;
-            this.quantity--;
-            this.total -= item.price;
-            return item;
-          }
-        });
       } else if (cartItem.quantity === 1) {
         // jika barangnya sisa 1
         this.items = this.items.filter((item) => item.id !== id);
         this.quantity--;
         this.total -= cartItem.price;
       }
+
+      // telusuri 1 1
+      this.items = this.items.map((item) => {
+        // jika bukan barang yang di klik
+        if (item.id !== id) {
+          return item;
+        } else {
+          item.quantity--;
+          item.total = item.price * item.quantity;
+          this.quantity--;
+          this.total -= item.price;
+          return item;
+        }
+      });
     },
   });
 });
 
-// konversi ke Rupiah
+// form falidation
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
+// kirim data ketika checkout di klik
+checkoutButton.addEventListener("click", async function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  // const message = formatMessage(objData);
+  // window.open('http://wa.me/6281243283970?text=' + encodeURIComponent(message));
+
+  // minta transaction token menggunakan ajax/fetch
+  try {
+    const response = await fetch("php/PlaceOrder.php", {
+      method: "POST",
+      body: data,
+    });
+    const token = await response.text();
+    // console.log(token);
+    window.snap.pay(token);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// format pesan watsapp
+const formatMessage = (obj) => {
+  return `Data Customer
+  Nama: ${obj.name}
+  Email: ${obj.email}
+  No HP: ${obj.phone}
+Data Pesanan
+${JSON.parse(obj.items).map(
+  (item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n`
+)}
+TOTAL: ${rupiah(obj.total)}
+Terima Kasih. `;
+};
+
+// conversi ke rupiah
 const rupiah = (number) => {
   return new Intl.NumberFormat("id-ID", {
     style: "currency",
